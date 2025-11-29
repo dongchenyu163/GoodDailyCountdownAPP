2dc3e3acbe4e58f1a98348f900f4739330f88c4d
dongchenyu165 <dongchenyu165@gmail.com>
2025-11-30 02:57:01 +0900

    fix(card-integrity): 统一卡片数据校正并在加载时应用

---
+++ composeApp/src/commonMain/kotlin/com/dlx/smartalarm/demo/features/cards/logic/CardIntegrity.kt
@@ -0,0 +1,5 @@
+package com.dlx.smartalarm.demo.features.cards.logic
+
+import com.dlx.smartalarm.demo.CardData
+
+fun fixCardData(card: CardData): CardData = com.dlx.smartalarm.demo.validateAndFixCardData(card)
